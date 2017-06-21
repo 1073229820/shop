0fc1d5f6cb3dedf605200ae20483df06842cf51d
zsttt159 <magephz@163.com>
2017-06-22 01:39:35 +0800

    goods list page

--- resources/views/admin/goods/lst.blade.php
+++ resources/views/admin/goods/lst.blade.php
@@ -114,15 +114,15 @@
                                     <td>
                                         <div class="visible-md visible-lg hidden-sm hidden-xs action-buttons">
                                             <a class="blue" href="#">
-                                                <i class="icon-zoom-in bigger-130"></i>
+                                                <i class="icon-zoom-in bigger-130" title="详情"></i>
                                             </a>
 
                                             <a class="green" href="#">
-                                                <i class="icon-pencil bigger-130"></i>
+                                                <i class="icon-pencil bigger-130" title="修改"></i>
                                             </a>
 
                                             <a class="red" href="#">
-                                                <i class="icon-trash bigger-130"></i>
+                                                <i class="icon-trash bigger-130" title="删除"></i>
                                             </a>
                                         </div>
 
