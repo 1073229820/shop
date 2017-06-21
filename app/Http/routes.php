3e3223cb2ad0cea696c5b7afe3a7a1fb5eba4490
ArchmageZ <magephz@163.com>
2017-06-21 10:37:11 +0800

    apply pjax

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'web'], function () {
+    Route::get('/', function () {
+        return view('admin/index');
+    });
+    Route::get('table', function () {
+        return view('admin/table');
+    });
+    Route::get('jqgrid', function () {
+        return view('admin/jqgrid');
+    });
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', function () {
+        return view('admin/index');
+    });
+    Route::get('table', function () {
+        return view('admin/table');
+    });
+    Route::get('jqgrid', function () {
+        return view('admin/jqgrid');
+    });
+});
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    这是首页
+@endsection
